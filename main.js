5613a950c3fba5956f71bd92b54c4b3a0de7eb64
Dumitru Postolachi <dumitru.postolachi@auto1.com>
2019-04-10 10:27:19 +0200

    Travis

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function createWindow() {
     })
 
     // and load the index.html of the app.
-    win.loadFile('https://inside.auto1-group.com/')
+    win.loadURL('https://inside.auto1-group.com/')
 
     win.webContents.on('did-finish-load', function () {
         win.webContents.executeJavaScript(`
